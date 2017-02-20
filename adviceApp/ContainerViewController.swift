ab1ff3d0898f6e77dc275ee9297b74d1f3ef1943
tameika <tameika.lawrence@gmail.com>
2017-02-20 15:30:29 -0500

    userdefaults setup

--- adviceApp/ContainerViewController.swift
+++ adviceApp/ContainerViewController.swift
@@ -9,26 +9,31 @@
 import UIKit
 
 class ContainerViewController: UIViewController {
-
-   
     
-   
+    var userDefaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.isHidden = true 
+        isUsersFirstTime()
     }
     
- 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func isUsersFirstTime() {
+        print(userDefaults.bool(forKey: "isFirstLaunch"))
+        
+        if userDefaults.bool(forKey: "isFirstLaunch") == true {
+            self.view.isHidden = false
+            userDefaults.set(false, forKey: "isFirstLaunch")
+            userDefaults.synchronize()
+            
+        } else if userDefaults.bool(forKey: "isFirstLaunch") == false {
+            self.view.isHidden = true
+        }
+        
     }
-    */
-
+    
+    
+    
 }
