b0949d70b195fbb454e2bb6d723cf7ea78c34ec8
tameika <tameika.lawrence@gmail.com>
2017-06-29 22:29:21 -0400

    advice vc edit

--- adviceApp/GiveGetAdviceViewController.swift
+++ adviceApp/GiveGetAdviceViewController.swift
@@ -345,7 +345,7 @@ class ViewController: UIViewController {
     }
 }
 
-// MARK : UITextFieldDelegate Method
+// MARK : Textfield Delegate Method
 
 extension ViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
