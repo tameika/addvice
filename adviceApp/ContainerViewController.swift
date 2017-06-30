e565ee6672afcebba57e9589c8150fefc3682601
tameika <tameika.lawrence@gmail.com>
2017-06-29 22:22:43 -0400

    container vc clean

--- adviceApp/ContainerViewController.swift
+++ adviceApp/ContainerViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ContainerViewController: UIViewController {
     
+    // MARK : UI Properties
+    
     @IBOutlet weak var blurView: UIVisualEffectView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    var userDefaults = UserDefaults.standard
+    // MARK : Logic Property 
     
+    var userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,8 @@ class ContainerViewController: UIViewController {
         setUpContainer()
         scrollView.delegate = self
     }
+    
+    // MARK : Set Up Container VC
     
     func hideContainerView() {
         blurView.effect = nil
@@ -40,11 +45,11 @@ class ContainerViewController: UIViewController {
         self.pageControl.currentPageIndicatorTintColor = UIColor.seafoamGreen
         self.pageControl.addTarget(self, action: #selector(self.changePage(sender:)), for: UIControlEvents.valueChanged)
     }
-        
+    
+    // MARK : User Defaults Logic
     
     func isUsersFirstTime() {
         print(userDefaults.bool(forKey: "isFirstLaunch"))
-        
         if userDefaults.bool(forKey: "isFirstLaunch") == true {
             userDefaults.set(false, forKey: "isFirstLaunch")
             userDefaults.synchronize()
@@ -55,14 +60,12 @@ class ContainerViewController: UIViewController {
                             self.blurView.effect = UIBlurEffect(style: .dark)
                             self.scrollView.alpha = 1.0
             })
-            
         } else if userDefaults.bool(forKey: "isFirstLaunch") == false {
             (self.parent as? ViewController)?.containerView.isHidden = true
-            
         }
-        
     }
     
+    // MARK : Dismiss Tutorial
     
     @IBAction func dismissTutorialView(_ sender: UITapGestureRecognizer) {
         UIView.animate(withDuration: 0.6,
@@ -74,13 +77,11 @@ class ContainerViewController: UIViewController {
             (self.parent as? ViewController)?.containerView.isHidden = true
         }
     }
-    
 }
 
+    // MARK : Scrollview Delegate Methods
+
 extension ContainerViewController: UIScrollViewDelegate {
-    
-    
-    
     func changePage(sender: AnyObject) -> () {
         let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
         scrollView.setContentOffset(CGPoint(x: x, y: 0), animated: true)
@@ -91,7 +92,5 @@ extension ContainerViewController: UIScrollViewDelegate {
         pageControl.currentPage = Int(pageNumber)
     }
 }
-
-
 
 
